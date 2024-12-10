52ea38bc699789578df5c3fa484039f84f354d81
지원 <ljiwon77733@gmail.com>
2024-12-11 05:23:57 +0900

    간단한 js 기능 추가
    - 검색창 form으로 해서 데이터 전달 가능하게 수정
    - 네비바 클릭 이벤트 추가
    - 네비바 클릭에 따라 달 바뀌도록 수정

--- js/main.js
+++ js/main.js
@@ -9,4 +9,29 @@ document.querySelectorAll('.icon-button').forEach(button => {
       }
     });
   });
+
+  // 모든 li 요소에 대해 클릭 이벤트 처리
+document.querySelectorAll('.nav_list').forEach(function (item) {
+    item.addEventListener('click', function () {
+      // 모든 li 요소에서 active 클래스 제거
+      document.querySelectorAll('.nav_list a').forEach(function (link) {
+        link.classList.remove('active');
+      });
+  
+      // 클릭한 li 요소에 active 클래스 추가
+      this.querySelector('a').classList.add('active');
+    });
+  });
+  
+  // 모든 li 요소에 클릭 이벤트 리스너 추가
+document.querySelectorAll('.nav_list a').forEach(function (item) {
+    item.addEventListener('click', function (event) {
+      // 클릭한 li의 텍스트 가져오기
+      const selectedMonth = event.target.textContent;
+  
+      // h4 요소에서 "12월" 부분을 클릭된 텍스트로 대체
+      const dateTextDiv = document.querySelector('.date-text-div h4');
+      dateTextDiv.textContent = selectedMonth + "의 게임추천 기록";
+    });
+  });
   
